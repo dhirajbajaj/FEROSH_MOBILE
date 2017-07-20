728ec591ff7be730741ec01ca85576eb937c905e
Hoang Hung <hth321@gmail.com>
2017-07-20 23:04:01 +0700

    update gulp build ios7

--- gulp/ios.js
+++ gulp/ios.js
@@ -4,7 +4,8 @@ import gulp from 'gulp';
 // If this doesn't work, while manual Xcode works, try:
 // 1) delete ios/build directory
 // 2) reset content and settings in iOS simulator
-gulp.task('ios', ['native'], bg(
-  'node', 'node_modules/react-native/local-cli/cli.js',
-  'run-ios', '--simulator', 'iPhone 5s',
-));
+gulp.task(
+  'ios',
+  ['native'],
+  bg('node', 'node_modules/react-native/local-cli/cli.js', 'run-ios', '--simulator', 'iPhone 7'),
+);

ac1a555d3855f8646d129f4fdf314d7825dce3b3
Hoang Hung <hth321@gmail.com>
2017-07-26 10:32:13 +0700

    upgrade packages and everything fine

--- src/common/configureMiddleware.js
+++ src/common/configureMiddleware.js
@@ -1,34 +1,27 @@
 // @flow weak
 import configureDeps from './configureDeps';
 import configureEpics from './configureEpics';
-import createLoggerMiddleware from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import { createEpicMiddleware } from 'redux-observable';
 
 // Like redux-thunk, but with just one argument.
 const injectMiddleware = deps => ({ dispatch, getState }) => next => action =>
-  next(typeof action === 'function'
-    ? action({ ...deps, dispatch, getState })
-    : action,
-  );
+  next(typeof action === 'function' ? action({ ...deps, dispatch, getState }) : action);
 
 const configureMiddleware = (initialState, platformDeps, platformMiddleware) => {
   const deps = configureDeps(initialState, platformDeps);
   const rootEpic = configureEpics(deps);
   const epicMiddleware = createEpicMiddleware(rootEpic);
 
-  const middleware = [
-    injectMiddleware(deps),
-    epicMiddleware,
-    ...platformMiddleware,
-  ];
+  const middleware = [injectMiddleware(deps), epicMiddleware, ...platformMiddleware];
 
-  const enableLogger = process.env.NODE_ENV !== 'production' && (
-    process.env.IS_BROWSER || initialState.device.isReactNative
-  );
+  const enableLogger =
+    process.env.NODE_ENV !== 'production' &&
+    (process.env.IS_BROWSER || initialState.device.isReactNative);
 
   // Logger must be the last middleware in chain.
   if (enableLogger) {
-    const logger = createLoggerMiddleware({
+    const logger = createLogger({
       collapsed: true,
     });
     middleware.push(logger);
